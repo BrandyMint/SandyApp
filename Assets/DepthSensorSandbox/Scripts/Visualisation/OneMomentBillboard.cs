dcf7d8284286960cdafe580b15bf16e0a9c98b68
konstantin <darkkon65@gmail.com>
2019-10-21 15:18:56 +0300

    + [#46] black background on load game + OnMomentBillboard on load scene with sandbox

---
+++ Assets/DepthSensorSandbox/Scripts/Visualisation/OneMomentBillboard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Linq;
+using DepthSensor.Buffer;
+using UnityEngine;
+
+namespace DepthSensorSandbox.Visualisation {
+    [RequireComponent(typeof(Canvas))]
+    public class OneMomentBillboard : MonoBehaviour {
+        private void Awake() {
+            var uiLayer = LayerMask.GetMask("UI");
+            var uiCamera = Camera.allCameras.First(c => (c.cullingMask & uiLayer) == uiLayer);
+            GetComponent<Canvas>().worldCamera = uiCamera;
+            DepthSensorSandboxProcessor.OnNewFrame += OnNewFrame;
+        }
+        
+        private void OnNewFrame(DepthBuffer depth, MapDepthToCameraBuffer map) {
+            if (depth.IsDepthValid()) {
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnDestroy() {
+            DepthSensorSandboxProcessor.OnNewFrame -= OnNewFrame;
+        }
+    }
+}
