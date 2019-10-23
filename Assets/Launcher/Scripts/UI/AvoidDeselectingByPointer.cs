aac574c172830124ebf85d1186a914432e5a5928
konstantin <darkkon65@gmail.com>
2019-10-23 22:39:35 +0300

    + avoid deselecting object by pointer

---
+++ Assets/Launcher/Scripts/UI/AvoidDeselectingByPointer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Launcher.UI {
+    public class AvoidDeselectingByPointer : MonoBehaviour {
+        private GameObject _lastSelected;
+        
+        private void Update() {
+            if (EventSystem.current == null)
+                return;
+            var curr = EventSystem.current.currentSelectedGameObject;
+            if (curr != null) {
+                _lastSelected = curr;
+            } else {
+                EventSystem.current.SetSelectedGameObject(_lastSelected);
+            }
+        }
+    }
+}
