4c65f14c25bbc812d6c2bc71996949895feec56d
konstantin <darkkon@mail.ru>
2019-11-06 09:52:58 +0300

    + tools/deploy usage and args

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-BUILD="../Builds"
+#usage: deploy.sh <platform> <build_path>
+#end
 
-if [[ ! -d "${BUILD}" ]]; then
-  echo "Path '${BUILD}' does not exist!"
+if [[ "$1" == "-h" ]]
+then
+	sed -n '2,/#end/ p' "$0"
+	exit 0
+fi
+
+PLATFORM=${1:-linux}
+BUILD=${2:-"../Builds"}
+
+if [[ ! -d "$BUILD" ]]; then
+  echo "Path '$BUILD' does not exist!"
   exit 1
 fi
 
-eval cd "${BUILD}"
+cd "$BUILD"
 VERSION=$(<version)
 echo ${VERSION}
 
-ARCHIVE_NAME=sandbox-linux-${VERSION}.zip
+ARCHIVE_NAME=sandbox-${PLATFORM}-${VERSION}.zip
 rm ${ARCHIVE_NAME}
 zip -r ${ARCHIVE_NAME} * --exclude *.zip
 
