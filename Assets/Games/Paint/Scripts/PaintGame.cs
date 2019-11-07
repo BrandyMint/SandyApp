c5d82859036ac0ae7f41a74a08e4640726b27f6a
konstantin <darkkon@mail.ru>
2019-11-07 18:55:28 +0300

    fix Game Paint

--- Assets/Games/Paint/Scripts/PaintGame.cs
+++ Assets/Games/Paint/Scripts/PaintGame.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using DepthSensorCalibration;
+﻿using DepthSensorCalibration;
 using DepthSensorSandbox.Visualisation;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -7,7 +6,6 @@ using UnityEngine.Rendering;
 namespace Games.Paint {
     public class PaintGame : MonoBehaviour {
         private static readonly int _HANDS_TEX = Shader.PropertyToID("_HandsTex");
-        private static readonly CameraEvent _PAINT_EVENT = CameraEvent.BeforeImageEffects;
         
         [SerializeField] private Camera _cam;
         [SerializeField] private SandboxMesh _sandbox;
@@ -16,38 +14,23 @@ namespace Games.Paint {
         //[SerializeField] private int _depthHeight = 128;
 
         private CameraRenderToTexture _renderHands;
-        private CommandBuffer _paintCmdBuffer;
+        private CameraRenderToTexture _renderPaint;
 
         private void Start() {
             _renderHands = _cam.gameObject.AddComponent<CameraRenderToTexture>();
+            _renderPaint = _cam.gameObject.AddComponent<CameraRenderToTexture>();
             //_renderPaint.MaxResolution = _depthHeight;
             _renderHands.Enable(_matDepth, RenderTextureFormat.R8, CameraEvent.BeforeForwardOpaque, OnNewDepthFrame, CreateCommandBufferDepth);
-
-            _paintCmdBuffer = CreatePaintBuffer();
-            _cam.AddCommandBuffer(_PAINT_EVENT, _paintCmdBuffer);
-            OneMomentBillboard.OnReady += OnReady;
+            _renderPaint.Enable(_matDepth, RenderTextureFormat.ARGB32, CameraEvent.BeforeImageEffects, null, CreateCommandBufferPaint);
         }
 
         private void OnDestroy() {
-            OneMomentBillboard.OnReady -= OnReady;
             if (_renderHands != null) {
                 _renderHands.Disable();
             }
-            if (_paintCmdBuffer != null) {
-                if (_cam != null)
-                    _cam.RemoveCommandBuffer(_PAINT_EVENT, _paintCmdBuffer);
-                _paintCmdBuffer.Dispose();
-                _paintCmdBuffer = null;
+            if (_renderPaint != null) {
+                _renderPaint.Disable();
             }
-        }
-
-        private void OnReady() {
-            StartCoroutine(NothingClearStartNextFrame());
-        }
-
-        private IEnumerator NothingClearStartNextFrame() {
-            yield return null;
-            _cam.clearFlags = CameraClearFlags.Depth;
         }
 
         private void CreateCommandBufferDepth(CommandBuffer cmb, Material mat, RenderTexture rt, RenderTargetIdentifier src) {
@@ -56,13 +39,9 @@ namespace Games.Paint {
             _sandbox.AddDrawToCommandBuffer(cmb, mat);
         }
 
-        private CommandBuffer CreatePaintBuffer() {
-            var cmb = new CommandBuffer {name ="paint"};
-            
-            var t = BuiltinRenderTextureType.CameraTarget;
-            cmb.Blit(t, t, _matPaint);
-            
-            return cmb;
+        private void CreateCommandBufferPaint(CommandBuffer cmb, Material mat, RenderTexture rt, RenderTargetIdentifier src) {
+            cmb.Blit(rt, src, _matPaint);
+            cmb.Blit(src, rt);
         }
 
         private void OnNewDepthFrame(RenderTexture t) {
