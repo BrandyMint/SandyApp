a899bbd234a22b1d5c74b103222271296b04e288
konstantin <darkkon65@gmail.com>
2019-11-10 17:16:06 +0300

    + [#69] moles games with dummy mole model

---
+++ Assets/Games/Moles/Scripts/Mole.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Games.Moles {
+    [RequireComponent(typeof(Renderer), typeof(ParticleSystem))]
+    public class Mole : MonoBehaviour {
+        private static readonly int _SHOW = Animator.StringToHash("show");
+        private static readonly int _HIDE = Animator.StringToHash("hide");
+
+        public int Player;
+        
+        private ParticleSystem _particles;
+        private Collider _collider;
+        private float _showTime;
+        private Animator _anim;
+
+        private void Awake() {
+            _particles = GetComponent<ParticleSystem>();
+            _collider = GetComponent<Collider>();
+            _anim = GetComponent<Animator>();
+
+            Hide(true);
+        }
+        
+        public MoleState State { get; private set; }
+
+        public void Show(float time, bool noAnimation = false) {
+            State = MoleState.TRANSITION_TO_SHOW;
+            _showTime = time;
+            if (noAnimation)
+                OnShowed();
+            else
+                _anim.SetTrigger(_SHOW);
+        }
+
+        public void Hide(bool noAnimation = false) {
+            StopCoroutine(nameof(Showing));
+            State = MoleState.TRANSITION_TO_HIDE;
+            if (noAnimation)
+                OnHided();
+            else
+                _anim.SetTrigger(_HIDE);
+        }
+
+        public void Bang() {
+            SetInteractive(false);
+            Hide();
+
+            //GetComponent<AudioSource>().Play();
+            _particles.Play();
+        }
+
+        private IEnumerator Showing(float time) {
+            yield return new WaitForSeconds(time);
+            Hide();
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        private void OnTransition() {
+            switch (State) {
+                case MoleState.TRANSITION_TO_SHOW:
+                    OnShowed();
+                    break;
+                case MoleState.TRANSITION_TO_HIDE:
+                    OnHided();
+                    break;
+            }
+        }
+
+        private void OnShowed() {
+            State = MoleState.SHOWED;
+            SetInteractive(true);
+            StartCoroutine(nameof(Showing), _showTime);
+        }
+
+        private void OnHided() {
+            State = MoleState.HIDED;
+            SetInteractive(false);
+        }
+
+        private void SetInteractive(bool interactive) {
+            //gameObject.layer = interactive ? _interactiveLayer : 0;
+            _collider.enabled = interactive;
+        }
+    }
+}
---
+++ Assets/Games/Moles/Scripts/MolesGame.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DepthSensorCalibration;
+using DepthSensorSandbox.Visualisation;
+using Games.Balloons;
+using Games.Common.Game;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Utilities;
+using Random = UnityEngine.Random;
+
+namespace Games.Moles {
+    public class MolesGame : MonoBehaviour {
+        [SerializeField] private Camera _cam;
+        [SerializeField] private Mole _tplMole;
+        [SerializeField] private GameField _gameField;
+        [SerializeField] private int _maxMolesShow = 3;
+        [SerializeField] private float _timeOffsetShown = 0.5f;
+        [SerializeField] private float _timeLife = 1f;
+        [SerializeField] private int _depthHeight = 64;
+        [SerializeField] private SandboxMesh _sandbox;
+        [SerializeField] private Material _matDepth;
+
+        private readonly List<Mole> _moles = new List<Mole>();
+        
+        private int _hitMask;
+        private CameraRenderToTexture _renderDepth;
+        private readonly DelayedDisposeNativeArray<byte> _depth = new DelayedDisposeNativeArray<byte>();
+        private int2 _depthSize; 
+        private float _initialMoleSize;
+        private int _score;
+        private bool _isGameStarted;
+
+        private void Start() {
+            _hitMask = LayerMask.GetMask("interactable");
+
+            _renderDepth = _cam.gameObject.AddComponent<CameraRenderToTexture>();
+            _renderDepth.MaxResolution = _depthHeight;
+            _renderDepth.Enable(_matDepth, RenderTextureFormat.R8, OnNewDepthFrame, CreateCommandBufferDepth);
+
+            _initialMoleSize = math.cmax(_tplMole.transform.localScale);
+            _tplMole.gameObject.SetActive(false);
+            
+            Prefs.Calibration.OnChanged += OnCalibrationChanged;
+            Prefs.Sandbox.OnChanged += OnCalibrationChanged;
+            OnCalibrationChanged();
+
+            GameEvent.OnStart += StartGame;
+            GameEvent.OnStop += StopGame;
+            
+            SpawnAll();
+        }
+
+        private void OnDestroy() {
+            GameEvent.OnStart -= StartGame;
+            GameEvent.OnStop -= StopGame;
+            
+            Prefs.Sandbox.OnChanged -= OnCalibrationChanged;
+            Prefs.Calibration.OnChanged -= OnCalibrationChanged;
+            if (_renderDepth != null) {
+                _renderDepth.Disable();
+            }
+            _depth.Dispose();
+        }
+
+        private void SpawnAll() {
+            var areas = SpawnArea.Areas.OrderBy(t => t.transform.localPosition.x);
+            var player = 0;
+            foreach (var area in areas) {
+                foreach (var spawn in area.Spawns) {
+                    _moles.Add(SpawnMole(player, spawn.position, spawn.rotation));
+                }
+                ++player;
+            }
+        }
+
+        private Mole SpawnMole(int player, Vector3 worldPos, Quaternion worldRot) {
+            var newMole = Instantiate(_tplMole, worldPos, worldRot, _tplMole.transform.parent);
+            newMole.Player = player;
+            newMole.gameObject.SetActive(true);
+            return newMole;
+        }
+
+        private IEnumerator MolesShowing(int player) {
+            var moles = _moles.Where(m => m.Player == player).ToArray();
+            yield return new WaitForSeconds(Random.value * _timeOffsetShown);
+            while (true) {
+                var countShown = moles.Count(m => m.State == MoleState.SHOWED);
+                if (countShown < _maxMolesShow) {
+                    if (moles.Where(m => m.State == MoleState.HIDED).TryRandom(out var mole)) {
+                        mole.Show(_timeLife);
+                    }
+                }
+                yield return new WaitForSeconds((0.5f + Random.value) * _timeOffsetShown);
+            }
+        }
+
+        private void Update() {
+            if (Input.GetMouseButtonDown(0)) {
+                var screen = new float2(_cam.pixelWidth, _cam.pixelHeight);
+                var pos = new float2(Input.mousePosition.x, Input.mousePosition.y);
+                Fire(pos / screen);
+            }
+        }
+
+        private void Fire(Vector2 viewPos) {
+            if (!_isGameStarted) return;
+            
+            var ray = _cam.ViewportPointToRay(viewPos);
+            if (Physics.Raycast(ray, out var hit, _cam.farClipPlane, _hitMask)) {
+                var mole = hit.collider.GetComponent<Mole>();
+                if (mole != null) {
+                    ++GameScore.PlayerScore[mole.Player];
+                    mole.Bang();
+                }
+            }
+        }
+
+        private void CreateCommandBufferDepth(CommandBuffer cmb, Material mat, RenderTexture rt, RenderTargetIdentifier src) {
+            cmb.SetRenderTarget(rt);
+            _sandbox.AddDrawToCommandBuffer(cmb, mat);
+        }
+
+        private void OnNewDepthFrame(RenderTexture t) {
+            _depthSize = new int2(t.width, t.height);
+            _renderDepth.RequestData(_depth, ProcessDepthFrame);
+        }
+        
+        private void ProcessDepthFrame() {
+            for (int x = 0; x < _depthSize.x; ++x) {
+                for (int y = 0; y < _depthSize.y; ++y) {
+                    if (_depth.o[x + y * _depthSize.x] > 0) {
+                        Fire(new float2(x, y) / _depthSize);
+                    }
+                }
+            }
+        }
+
+        private void OnCalibrationChanged() {
+            var cam = _cam.GetComponent<SandboxCamera>();
+            if (cam != null) {
+                cam.OnCalibrationChanged();
+                SetSizes(Prefs.Sandbox.ZeroDepth);
+            } else {
+                SetSizes(1f); //for testing
+            }
+        }
+
+        private void SetSizes(float dist) {
+            var verticalSize = MathHelper.IsoscelesTriangleSize(dist, _cam.fieldOfView);
+            var size = verticalSize * _initialMoleSize;
+            _tplMole.transform.localScale = Vector3.one * size;
+            foreach (var mole in _moles) {
+                mole.transform.localScale = Vector3.one * size;
+            }
+            _gameField.AlignToCamera(_cam, dist);
+            _gameField.SetWidth(0f);
+        }
+
+        private void ResetMoles() {
+            foreach (var moles in _moles) {
+                moles.Hide(true);
+            }
+        }
+
+        private void StartGame() {
+            ResetMoles();
+            _isGameStarted = true;
+            for (int i = 0; i < _moles.Count; ++i) {
+                GameScore.PlayerScore[i] = 0;
+                StartCoroutine(nameof(MolesShowing), i);
+            }
+            
+        }
+
+        private void StopGame() {
+            _isGameStarted = false;
+            StopCoroutine(nameof(MolesShowing));
+        }
+    }
+}
