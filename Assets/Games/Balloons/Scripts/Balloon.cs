77d89b91eb9141a2216269ed876a995759428147
konstantin <darkkon65@gmail.com>
2019-11-13 13:01:51 +0300

    * refactoring: extracted RandomColorRenderer from Ballon and GeometryInteractable

--- Assets/Games/Balloons/Scripts/Balloon.cs
+++ Assets/Games/Balloons/Scripts/Balloon.cs
@@ -1,34 +1,18 @@
 ﻿using System;
 using System.Collections;
+using Games.Common;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace Games.Balloons {
-    [RequireComponent(typeof(Renderer), typeof(ParticleSystem))]
+    [RequireComponent(typeof(RandomColorRenderer), typeof(ParticleSystem))]
     public class Balloon : MonoBehaviour {
-        private static readonly int _COLOR = Shader.PropertyToID("_Color");
         public static event Action<Balloon> OnDestroyed;
         public static event Action<Balloon, Collision> OnCollisionEntered;
-
-        private Renderer _r;
+        
         private ParticleSystem _particles;
 
         private void Awake() {
-            _r = GetComponent<Renderer>();
-            
-            var startColor = _r.material.GetColor(_COLOR);
-            Color.RGBToHSV(startColor, out _, out var s, out var v);
-            startColor = Color.HSVToRGB(Random.value, s, v);
-
-            
-            var props = new MaterialPropertyBlock();
-            _r.GetPropertyBlock(props);
-            props.SetColor(_COLOR, startColor);
-            _r.SetPropertyBlock(props);
-
             _particles = GetComponent<ParticleSystem>();
-            var module = _particles.main; 
-            module.startColor = startColor;
         }
 
         private void OnDestroy() {
@@ -37,12 +21,15 @@ namespace Games.Balloons {
 
         public void Bang() {
             gameObject.layer = 0;
-            _r.enabled = false;
+            GetComponent<Renderer>().enabled = false;
             GetComponent<Collider>().enabled = false;
             GetComponent<ConstantForce>().enabled = false;
             var body = GetComponent<Rigidbody>();
             body.velocity = Vector3.zero;
             body.angularVelocity = Vector3.zero;
+            
+            var module = _particles.main; 
+            module.startColor = GetComponent<RandomColorRenderer>().GetColor();
             
             GetComponent<AudioSource>().Play();
             StartCoroutine(PlayParticlesAndDead());
---
+++ Assets/Games/Common/Scripts/RandomColorRenderer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Games.Common {
+    [RequireComponent(typeof(Renderer))]
+    public class RandomColorRenderer : MonoBehaviour {
+        private static readonly int _COLOR = Shader.PropertyToID("_Color");
+
+        private Color _color;
+        private Renderer _r;
+
+        private void Awake() {
+            SetRandomColor();
+        }
+
+        public void SetRandomColor() {
+            _r = GetComponent<Renderer>();
+            _color = _r.material.GetColor(_COLOR);
+            Color.RGBToHSV(_color, out _, out var s, out var v);
+            _color = Color.HSVToRGB(Random.value, s, v);
+            
+            var props = new MaterialPropertyBlock();
+            _r.GetPropertyBlock(props);
+            props.SetColor(_COLOR, _color);
+            _r.SetPropertyBlock(props);
+        }
+
+        public Color GetColor() {
+            return _color;
+        }
+    }
+}
