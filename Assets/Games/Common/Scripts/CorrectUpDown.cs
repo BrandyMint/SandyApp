5e0178563b4ad4a4fcf473f4053e376648ba53b2
konstantin <darkkon65@gmail.com>
2019-12-05 20:57:16 +0300

    fix game objects positions and sizes relative flip ui or sandbox

--- Assets/Games/Common/Scripts/CorrectUpDown.cs
+++ Assets/Games/Common/Scripts/CorrectUpDown.cs
@@ -2,6 +2,15 @@
 
 namespace Games.Common {
     public class CorrectUpDown : MonoBehaviour {
+        public enum CorrectMethod {
+            SCALE,
+            POSITION,
+            ROTATION
+        }
+        
+        [SerializeField] private bool _byUIFlip = true;
+        [SerializeField] private bool _bySandboxFlip = true;
+        [SerializeField] private CorrectMethod _method = CorrectMethod.SCALE; 
         private float _prevY = 1f;
         
         private void Awake() {
@@ -14,10 +23,25 @@ namespace Games.Common {
         }
 
         private void OnAppChanged() {
-            var scale = transform.localScale;
-            var y = Prefs.App.FlipVertical ? -1f : 1f;
-            scale.y *= y * _prevY;
-            transform.localScale = scale;
+            var flip = (Prefs.App.FlipVertical && _byUIFlip) ^ (Prefs.App.FlipVerticalSandbox && _bySandboxFlip);
+            var y = flip ? -1f : 1f;
+            switch (_method) {
+                case CorrectMethod.SCALE:
+                    var scale = transform.localScale;
+                    scale.y *= y * _prevY;
+                    transform.localScale = scale;
+                    break;
+                case CorrectMethod.POSITION:
+                    var pos = transform.localPosition;
+                    pos.y *= y * _prevY;
+                    transform.localPosition = pos;
+                    break;
+                case CorrectMethod.ROTATION:
+                    var r = y * _prevY < 1f ? Quaternion.AngleAxis(180f, Vector3.back) : Quaternion.identity;
+                    var rot = transform.localRotation;
+                    transform.localRotation = rot * r;
+                    break;
+            }
             _prevY = y;
         }
     }
