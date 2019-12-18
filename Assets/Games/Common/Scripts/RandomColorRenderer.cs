9e837a3cb3985cb4a7cda7efa251f27d8960bf61
konstantin <darkkon65@gmail.com>
2019-12-18 11:35:37 +0300

    + RamdomColorRenderer support multimaterial renderers

--- Assets/Games/Common/Scripts/RandomColorRenderer.cs
+++ Assets/Games/Common/Scripts/RandomColorRenderer.cs
@@ -5,6 +5,7 @@ namespace Games.Common {
     [RequireComponent(typeof(Renderer))]
     public class RandomColorRenderer : MonoBehaviour {
         private static readonly int _COLOR = Shader.PropertyToID("_Color");
+        [SerializeField] private int _onlyForMaterialId = -1;
 
         private Color _color;
         private Renderer _r;
@@ -18,11 +19,19 @@ namespace Games.Common {
             _color = _r.material.GetColor(_COLOR);
             Color.RGBToHSV(_color, out _, out var s, out var v);
             _color = Color.HSVToRGB(Random.value, s, v);
-            
-            var props = new MaterialPropertyBlock();
-            _r.GetPropertyBlock(props);
-            props.SetColor(_COLOR, _color);
-            _r.SetPropertyBlock(props);
+
+            if (_onlyForMaterialId >= 0 && _onlyForMaterialId < _r.materials.Length) {
+                var materials = _r.materials;
+                var mat = new Material(materials[_onlyForMaterialId]);
+                mat.SetColor(_COLOR, _color);
+                materials[_onlyForMaterialId] = mat;
+                _r.materials = materials;
+            } else {
+                var props = new MaterialPropertyBlock();
+                _r.GetPropertyBlock(props);
+                props.SetColor(_COLOR, _color);
+                _r.SetPropertyBlock(props);
+            }
         }
 
         public Color GetColor() {
