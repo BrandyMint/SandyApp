ac615fcf0b035498c64e4fc049ac19d227b361f2
konstantin <darkkon65@gmail.com>
2019-12-27 05:18:25 +0300

    + Car scripts and models

---
+++ Assets/Games/Common/Car/Scripts/BezierWayPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BezierSolution;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace UnityStandardAssets.Vehicles.Car {
+    public class BezierWayPoint : MonoBehaviour {
+        public BezierSpline spline;
+        [SerializeField] private Transform _wayPoint;
+        [SerializeField] private float _accuracy = 200f;
+
+        private Vector3 _initialDist; 
+
+        private void Start() {
+            _initialDist = _wayPoint.localPosition;
+        }
+
+        private void Update() {
+            if (spline != null) {
+                var dist = transform.TransformVector(_initialDist).magnitude;
+                spline.FindNearestPointTo(transform.position, out var t, _accuracy);
+                _wayPoint.position = spline.MoveAlongSpline(ref t, dist);
+                _wayPoint.rotation = quaternion.LookRotation(spline.GetTangent(t), transform.up);
+            }
+        }
+    }
+}
---
+++ Assets/Games/Common/Scripts/LightRangeScale.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Games.Common {
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Light))]
+    public class LightRangeScale : MonoBehaviour {
+        public float _localRange = 10f;
+
+        private Light _light;
+
+        private void Awake() {
+            _light = GetComponent<Light>();
+        }
+        
+#if UNITY_EDITOR
+        private void OnEnable() {
+            Awake();
+        }
+#endif
+
+#if UNITY_EDITOR
+        private void LateUpdate() {
+            if (!UnityEditor.EditorApplication.isPlaying)
+                FixedUpdate();
+        }
+#endif
+
+        private void FixedUpdate() {
+            var range = transform.TransformVector(Vector3.forward * _localRange).magnitude;
+            _light.range = range;
+        }
+    }
+}
