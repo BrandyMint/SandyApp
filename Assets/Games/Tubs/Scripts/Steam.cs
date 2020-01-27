f9c1dfb2a9d8377c75068caab90c13575f11967a
konstantin <darkkon@mail.ru>
2020-01-27 22:07:51 +0300

    + [#85] Steam fix clockwise in game tube

--- Assets/Games/Tubs/Scripts/Steam.cs
+++ Assets/Games/Tubs/Scripts/Steam.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 namespace Games.Tubs {
     public class Steam : InteractableModel {
+        private static readonly int _FILL = Shader.PropertyToID("_Fill");
+        
         [SerializeField] private ParticleSystem _steamParticles;
         [SerializeField] private float _timeToFix = 2f;
+        [SerializeField] private Renderer _clock;
 
         private float _steamFactor = 1f;
         private float _maxSteam;
+        private MaterialPropertyBlock _clockProps;
+
+        protected override void Awake() {
+            _clockProps = new MaterialPropertyBlock();
+            _clock.gameObject.SetActive(true);
+            base.Awake();
+        }
 
         private void Start() {
             _maxSteam = _steamParticles.main.startLifetime.constant;
@@ -23,13 +33,18 @@ namespace Games.Tubs {
             var module = _steamParticles.main;
             var startLifetime = module.startLifetime;
             _steamParticles.Play();
+            var time = 1f;
 
             while (true) {
-                startLifetime.constant += _maxSteam / _timeToFix * _steamFactor * Time.deltaTime;
-                if (startLifetime.constant < 0f)
-                    break;
-                startLifetime.constant = Mathf.Clamp(startLifetime.constant, 0f, _maxSteam);
+                time += _steamFactor * Time.deltaTime / _timeToFix;
+                time = Mathf.Clamp01(time);
+                startLifetime.constant = time * _maxSteam;
+                _clock.GetPropertyBlock(_clockProps);
+                _clockProps.SetFloat(_FILL, time);
+                _clock.SetPropertyBlock(_clockProps);
                 module.startLifetime = startLifetime;
+                if (time <= 0f)
+                    break;
                 yield return null;
             }
             
