a46474a6626e88244c57ac40d0ad0c4caf446d30
konstantin <darkkon@mail.ru>
2020-02-12 18:49:04 +0300

    fix SystemInfo.deviceUniqueIdentifier may not unique in Linux

--- Assets/SimpleProtect/Scripts/Protection.cs
+++ Assets/SimpleProtect/Scripts/Protection.cs
@@ -1,5 +1,6 @@
 #if BUILD_PROTECT_COPY || BUILD_ACTIVATOR
 using System;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
 using UnityEngine;
@@ -11,12 +12,37 @@ namespace SimpleProtect {
         }
 
         public static string GenerateKey() {
-            var unique = SystemInfo.deviceUniqueIdentifier;
-            var s = unique + Phrase.Get() + unique;
+            var unique = GetDeviceUniqueId();
+            var s = unique + Phrase.Get();
             using (var sha = SHA512.Create()) {
                 var hash = sha.ComputeHash(Encoding.Default.GetBytes(s));
                 return BitConverter.ToString(hash);
             }
+        }
+
+        private static string GetDeviceUniqueId() {
+            var id = SystemInfo.deviceUniqueIdentifier;
+#if UNITY_STANDALONE_LINUX
+            //TODO: SystemInfo.deviceUniqueIdentifier not unique in Linux, workaround with 'cat /etc/fstab'  
+            try {
+                var p = new Process {
+                    StartInfo = {
+                        FileName = "cat",
+                        Arguments = "/etc/fstab",
+                        CreateNoWindow = true,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false
+                    }
+                };
+                p.Start();
+                p.WaitForExit(10000);
+                id += p.StandardOutput.ReadToEnd();
+            } catch {
+                // ignored
+            }
+#endif
+            return id;
         }
     }
 }
